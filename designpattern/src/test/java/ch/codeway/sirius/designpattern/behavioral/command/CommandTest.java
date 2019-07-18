1444ed47f828f72c2f479f06361f6d27cde22a0b
s.rinaudo@widegroup.ch <s.rinaudo@widegroup.ch>
2019-07-18 10:34:34 +0200

    Add Command pattern example

---
+++ designpattern/src/test/java/ch/codeway/sirius/designpattern/behavioral/command/CommandTest.java
@@ -0,0 +1,75 @@
+package ch.codeway.sirius.designpattern.behavioral.command;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Test example in Command Pattern Example
+ */
+public class CommandTest {
+
+    private Light bedroomLight;
+    private Light kitchenLight;
+    private Switch lightSwitch;
+
+    @Before
+    public void setUp() {
+        bedroomLight = new Light();
+        kitchenLight = new Light();
+
+        lightSwitch = new Switch();
+    }
+
+    @Test
+    public void turnOffAllLights() {
+        // Receivers
+        bedroomLight.on();
+        kitchenLight.on();
+
+        // Command
+        Command turnOffCommand = new TurnOffAllLightsCommand(Arrays.asList(bedroomLight, kitchenLight));
+
+        // Invoker
+        lightSwitch.storeAndExecute(turnOffCommand);
+
+        // Assertion
+        Assertions.assertThat(bedroomLight.isOn()).isFalse();
+        Assertions.assertThat(kitchenLight.isOn()).isFalse();
+    }
+
+    @Test
+    public void toggleAllLights() {
+        // Receivers
+        bedroomLight.off();
+        kitchenLight.on();
+
+        // Command
+        Command toggleAllLightsCommand = new ToggleAllLightsCommand(Arrays.asList(bedroomLight, kitchenLight));
+
+        // Invoker
+        lightSwitch.storeAndExecute(toggleAllLightsCommand);
+
+        // Assertion
+        Assertions.assertThat(bedroomLight.isOn()).isTrue();
+        Assertions.assertThat(kitchenLight.isOn()).isFalse();
+    }
+
+    @Test
+    public void toggleSingleLight() {
+        // Receivers
+        kitchenLight.on();
+
+        // Command
+        Command toggleCommand = new ToggleCommand(kitchenLight);
+
+        // Invoker
+        lightSwitch.storeAndExecute(toggleCommand);
+
+        // Assertion
+        Assertions.assertThat(kitchenLight.isOn()).isFalse();
+    }
+
+}
