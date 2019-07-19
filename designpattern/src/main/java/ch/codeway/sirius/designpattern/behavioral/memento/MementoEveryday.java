4771dd62dca6a8e43b7004b73e4fda04bda7d2e8
s.rinaudo@widegroup.ch <s.rinaudo@widegroup.ch>
2019-07-19 17:20:54 +0200

    Add Memento pattern example

---
+++ designpattern/src/main/java/ch/codeway/sirius/designpattern/behavioral/memento/MementoEveryday.java
@@ -0,0 +1,38 @@
+package ch.codeway.sirius.designpattern.behavioral.memento;
+
+import java.io.*;
+
+/**
+ * Memento example for serialization and deserialization
+ */
+public class MementoEveryday {
+
+    public static Employee deserialize() {
+        Employee emp = null;
+        try {
+            FileInputStream fileIn = new FileInputStream("/tmp/employee.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            emp = (Employee) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return emp;
+    }
+
+    public static void serialize(Employee emp) {
+
+        try {
+            FileOutputStream fileOut = new FileOutputStream("/tmp/employee.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(emp);
+            out.close();
+            fileOut.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+}
