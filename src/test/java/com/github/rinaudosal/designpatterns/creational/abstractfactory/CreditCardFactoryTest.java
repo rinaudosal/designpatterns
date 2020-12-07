91c86beeb1147ebcfe81c3b6af9f48834d4e39a4
Salvatore Rinaudo <rinaudo.sal@gmail.com>
2020-12-07 12:45:52 +0100

    Removed unused import

--- src/test/java/com/github/rinaudosal/designpatterns/creational/abstractfactory/CreditCardFactoryTest.java
+++ src/test/java/com/github/rinaudosal/designpatterns/creational/abstractfactory/CreditCardFactoryTest.java
@@ -1,6 +1,5 @@
 package com.github.rinaudosal.designpatterns.creational.abstractfactory;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
