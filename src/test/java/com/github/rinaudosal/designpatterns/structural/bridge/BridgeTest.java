2f607c2d77eb743937f2765eb00c98b092b3da1e
Salvatore Rinaudo <rinaudo.sal@gmail.com>
2020-12-07 16:20:42 +0100

    Add HtmlFormatter

--- src/test/java/com/github/rinaudosal/designpatterns/structural/bridge/BridgeTest.java
+++ src/test/java/com/github/rinaudosal/designpatterns/structural/bridge/BridgeTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BridgeTest {
 
     @Test
-    void movieFormatterShouldWork() {
+    void printerFormatterShouldWork() {
         Movie movie = new Movie();
         movie.setTitle("John Wick");
         movie.setClassification("Action");
@@ -19,5 +19,20 @@ public class BridgeTest {
         String moviePrinted = moviePrinter.print(formatter);
 
         assertThat(moviePrinted).isEqualTo("Action\nTitle: John Wick\nRuntime: 2:15\nYear: 2014\n");
+    }
+
+    @Test
+    void htmlFormatterShouldWork() {
+        Movie movie = new Movie();
+        movie.setTitle("John Wick");
+        movie.setClassification("Action");
+        movie.setRuntime("2:13");
+        movie.setYear("2014");
+
+        Formatter formatter = new HtmlFormatter();
+        Printer moviePrinter = new MoviePrinter(movie);
+        String moviePrinted = moviePrinter.print(formatter);
+
+        assertThat(moviePrinted).isEqualTo("<table><th>Classification</th><th>Action</th><tr><td>Title</td><td>John Wick</td></tr><tr><td>Runtime</td><td>2:13</td></tr><tr><td>Year</td><td>2014</td></tr></table>");
     }
 }
