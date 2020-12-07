fe6497c91c1dd00537f637ee611552bc7ec8dc10
Salvatore Rinaudo <rinaudo.sal@gmail.com>
2020-12-07 15:01:59 +0100

    Add missing tests. Add badges for build and coverage

--- src/test/java/com/github/rinaudosal/designpatterns/DesignpatternApplicationTests.java
+++ src/test/java/com/github/rinaudosal/designpatterns/DesignpatternApplicationTests.java
@@ -3,11 +3,12 @@ package com.github.rinaudosal.designpatterns;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DesignpatternApplication.class)
 public class DesignpatternApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    // Test class added ONLY to cover main() invocation not covered by application tests.
+    @Test
+    public void main() {
+        DesignpatternApplication.main(new String[]{});
+    }
 }
